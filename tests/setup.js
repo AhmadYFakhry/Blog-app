a12a2faa7347d1f26fb7f410c5277f6cc9a10862
Ahmad Fakhry <afakhry98@gmail.com>
2020-01-02 16:44:09 +0300

    Tests

--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,7 @@ require("../models/User");
 
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
+jest.setTimeout(30000);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
