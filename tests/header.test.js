cd369b0af76840ff47b9fec667890aa2ed5f131c
Ahmad Fakhry <afakhry98@gmail.com>
2020-01-02 16:41:25 +0300

    Testing

--- tests/header.test.js
+++ tests/header.test.js
@@ -5,7 +5,7 @@ let page;
 beforeEach(async () => {
   page = await Page.build();
   await page.goto("http://localhost:3000");
-}, 50000);
+});
 afterEach(async () => {
   await page.close();
 });
@@ -13,7 +13,7 @@ afterEach(async () => {
 test("The header has the correct title", async () => {
   const text = await page.getContentsOf("a.brand-logo");
   expect(text).toEqual("Blogster");
-}, 10000);
+});
 
 test("Starting the OAuth flow", async () => {
   await page.click(".right a");
@@ -25,4 +25,4 @@ test("When signed in, shows logout button", async () => {
   await page.login();
   const text = await page.getContentsOf('a[href="/auth/logout"]');
   expect(text).toEqual("Logout");
-}, 50000);
+});
