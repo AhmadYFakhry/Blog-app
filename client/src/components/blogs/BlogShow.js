fda861ff700411d1f98eb7d84c524331587473b4
Ahmad Fakhry <afakhry98@gmail.com>
2020-01-04 22:29:19 +0300

    Added env variables

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -11,9 +11,8 @@ class BlogShow extends Component {
   }
 
   renderImage() {
-    console.log(process.env.S3_BUCKET_NAME);
     if (this.props.blog.imgURL) {
-      return <img alt="added by user" src={'https://ahmadyfakhry-blog-app.s3.ca-central-1.amazonaws.com/' + this.props.blog.imgURL} />
+      return <img alt="added by user" src={process.env.S3_BUCKET_URL + this.props.blog.imgURL} />
     }
   }
 
